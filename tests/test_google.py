f67a4d2074e49d50e4d14acb9c5fd1be35adf6b6
Sukhanov Pavel <ssuxxarr@gmail.com>
2025-01-19 22:52:55 +0300

    Move wait until to conditions function

--- tests/test_google.py
+++ tests/test_google.py
@@ -1,21 +1,14 @@
-from selenium import webdriver
-from selenium.common import WebDriverException
 from selenium.webdriver import Keys
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from webdriver_manager.chrome import ChromeDriverManager
+from config import driver
 
-from webdriver_demo.conditions import click_on_element, type_to_element, number_of_elements
+from webdriver_demo.conditions import click_on, number_of_elements, type_to, asser_that
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-wait = WebDriverWait(driver, timeout=5, ignored_exceptions=(WebDriverException,))
+driver = driver
 
 driver.get('https://www.ecosia.org/')
-driver.find_element(By.CSS_SELECTOR, '[name=q]').send_keys()
 
-wait.until(type_to_element('[name=q]', 'selene yashaka' + Keys.ENTER))
-wait.until(click_on_element('[data-test-id="mainline-result-web"] .result__header :nth-of-type(1)'))
-wait.until(click_on_element('#pull-requests-tab'))
+type_to('[name=q]', 'selene yashaka' + Keys.ENTER)
+click_on('[data-test-id="mainline-result-web"] .result__header :nth-of-type(1)')
+click_on('#pull-requests-tab')
 
-number_of_pulls = number_of_elements('[id^=issue_]:not([id$=_link])', 9)
+asser_that(number_of_elements('[id^=issue_]:not([id$=_link])'), 9)
--- webdriver_demo/conditions.py
+++ webdriver_demo/conditions.py
@@ -1,9 +1,10 @@
 from typing import Tuple
-
+from config import wait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support.expected_conditions import _element_if_visible
 
+wait = wait
 
 def to_locator(selector: str) -> Tuple[str, str]:
     return (By.XPATH, selector) if (
@@ -16,37 +17,23 @@ def to_locator(selector: str) -> Tuple[str, str]:
 
 
 def element(selector):
-    def command(driver):
+    def command(driver: WebDriver):
         return _element_if_visible(driver.find_element(*to_locator(selector)))
 
-    return command
+    wait.until(command)
 
 
-def click_on_element(selector):
-    selector_type = By.XPATH if (
-            selector.startswith('/') or
-            selector.startswith('//') or
-            selector.startswith('./') or
-            selector.startswith('..') or
-            selector.startswith('(')
-    ) else By.CSS_SELECTOR
+def click_on(selector):
 
     def command(driver: WebDriver):
         webelement = driver.find_element(*to_locator(selector))
         webelement.click()
         return webelement
 
-    return command
+    wait.until(command)
 
 
-def type_to_element(selector, value):
-    selector_type = By.XPATH if (
-            selector.startswith('/') or
-            selector.startswith('//') or
-            selector.startswith('./') or
-            selector.startswith('..') or
-            selector.startswith('(')
-    ) else By.CSS_SELECTOR
+def type_to(selector, value):
 
     def command(driver: WebDriver):
         webelement = driver.find_element(*to_locator(selector))
@@ -54,20 +41,14 @@ def type_to_element(selector, value):
         webelement.send_keys(value)
         return webelement
 
-    return command
+    wait.until(command)
 
 
-def number_of_elements(selector, value: int):
-    selector_type = By.XPATH if (
-            selector.startswith('/') or
-            selector.startswith('//') or
-            selector.startswith('./') or
-            selector.startswith('..') or
-            selector.startswith('(')
-    ) else By.CSS_SELECTOR
+def number_of_elements(selector):
+    def command(driver: WebDriver):
+        return len(driver.find_elements(*to_locator(selector)))
 
-    def predicate(driver: WebDriver):
-        webelements = driver.find_elements(*to_locator(selector))
-        return len(webelements) == value
+    return wait.until(command)
 
-    return predicate
+def asser_that(actual_value, value):
+    assert actual_value == value
